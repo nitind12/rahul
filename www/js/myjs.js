5ecd8843d0b79db2b3938755ff9c8d5a7fa51012
Dr. Nitin Deepak <nitin.d12@gmail.com>
2017-06-02 18:58:18 +0530

    update script
    update script

--- www/js/myjs.js
+++ www/js/myjs.js
@@ -1,30 +1,60 @@
 $(function(){
 	$('#txtP_Moisture').blur(function(){
-		if($('#txtP_Moisture').val() != ''){
-			if(parseInt($('#txtP_Moisture').val()) >= 14){
-				$('#txtP_D').val(parseInt($('#txtP_Moisture').val()) - 14);	
+		if($.trim($('#txtP_Moisture').val()) != ''){
+			if(parseFloat($('#txtP_Moisture').val()) >= 14){
+				$('#txtP_D').val(parseFloat($('#txtP_Moisture').val()) - 14);	
 			} else {
 				$('#txtP_D').val('0');
 			}
 		}
-		if($('#txtO_Moisture').val() != ''){
-			if(parseInt($('#txtO_Moisture').val()) >= 14){
-				$('#txtO_D').val(parseInt($('#txtO_Moisture').val()) - 14);	
+		if($.trim($('#txtO_Moisture').val()) != ''){
+			if(parseFloat($('#txtO_Moisture').val()) >= 14){
+				$('#txtO_D').val(parseFloat($('#txtO_Moisture').val()) - 14);	
 			} else {
 				$('#txtO_D').val('0');
 			}	
 		}
 		if($.trim($('#txtP_C').val()) != '' && $.trim($('#txtP_D').val()) != ''){
-			p_c = parseInt($.trim($('#txtP_C').val()));
-			p_d = parseInt($.trim($('#txtP_D').val()));
+			p_c = parseFloat($.trim($('#txtP_C').val()));
+			p_d = parseFloat($.trim($('#txtP_D').val()));
 			p_result = (p_c-(p_c*p_d)/100);
 			$('#txtP_E').val(p_result.toFixed(2));
 		}
 		if($.trim($('#txtO_C').val()) != '' && $.trim($('#txtO_D').val()) != ''){
-			o_c = parseInt($.trim($('#txtO_C').val()));
-			o_d = parseInt($.trim($('#txtO_D').val()));
+			o_c = parseFloat($.trim($('#txtO_C').val()));
+			o_d = parseFloat($.trim($('#txtO_D').val()));
 			o_result = (o_c-(o_c*o_d)/100);
 			$('#txtO_E').val(o_result.toFixed(2));
+		}
+		if($.trim($('#txtP_A').val())!= '' && $.trim($('#txtP_B').val())!='' && $.trim($('#txtP_E').val())!=''){
+			p_a = parseFloat($.trim($('#txtP_A').val()));
+			p_b = parseFloat($.trim($('#txtP_B').val()));
+			p_e = parseFloat($.trim($('#txtP_E').val()));
+			p_result = (p_e/(p_a*p_b))
+			$('#txtP_F').val(p_result.toFixed(4));
+		}
+		if($.trim($('#txtO_A').val())!= '' && $.trim($('#txtO_B').val())!='' && $.trim($('#txtO_E').val())!=''){
+			o_a = parseFloat($.trim($('#txtO_A').val()));
+			o_b = parseFloat($.trim($('#txtO_B').val()));
+			o_e = parseFloat($.trim($('#txtO_E').val()));
+			o_result = (o_e/(o_a*o_b))
+			$('#txtO_F').val(o_result.toFixed(4));
+		}
+		if($.trim($('#txtP_F').val())!=''){
+			p_f = parseFloat($.trim($('#txtP_F').val()));
+			p_result = p_f * 4046;
+			$('#txtP_G').val(p_result.toFixed(4));
+		}
+		if($.trim($('#txtO_F').val())!=''){
+			o_f = parseFloat($.trim($('#txtO_F').val()));
+			o_result = o_f * 4046;
+			$('#txtO_G').val(o_result.toFixed(4));	
+		}
+		if($.trim($('#txtP_G').val()) != '' && $.trim($('#txtO_G').val()) != ''){
+			p = parseFloat($.trim($('#txtP_G').val()));
+			o = parseFloat($.trim($('#txtO_G').val()));
+			diff_ = p - o; 
+			$('#total_difference').html(diff_.toFixed(4));
 		}
 	});
 
@@ -47,12 +77,6 @@ $(function(){
 		$('#txtP_Moisture').blur();
 	});
 	$('#txtO_C').blur(function(){
-		$('#txtP_Moisture').blur();
-	});
-	$('#txtP_D').blur(function(){
-		$('#txtP_Moisture').blur();
-	});
-	$('#txtO_D').blur(function(){
 		$('#txtP_Moisture').blur();
 	});
 });
