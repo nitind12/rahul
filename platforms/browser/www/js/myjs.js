ae6d9a528a1649d366c68600ec4256a9a054e1b8
Dr. Nitin Deepak <nitin.d12@gmail.com>
2017-06-02 13:25:23 +0530

    updates
    updates

--- platforms/browser/www/js/myjs.js
+++ platforms/browser/www/js/myjs.js
@@ -1,3 +1,22 @@
 $(function(){
-	
+	$('#txtP_Moisture').blur(function(){
+		if($('#txtP_Moisture').val() != ''){
+			if(parseInt($('#txtP_Moisture').val()) >= 14){
+				$('#txtP_D').val(parseInt($('#txtP_Moisture').val()) - 14);	
+			} else {
+				$('#txtP_D').val('0');
+			}
+		}
+		if($('#txtO_Moisture').val() != ''){
+			if(parseInt($('#txtO_Moisture').val()) >= 14){
+				$('#txtO_D').val(parseInt($('#txtO_Moisture').val()) - 14);	
+			} else {
+				$('#txtO_D').val('0');
+			}	
+		}
+	});
+
+	$('#txtO_Moisture').blur(function(){
+		$('#txtP_Moisture').blur();
+	});
 });
