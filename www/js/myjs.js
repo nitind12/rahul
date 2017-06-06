7f123280fae30cc55f477a625017a8e78c38b750
Dr. Nitin Deepak <nitin.d12@gmail.com>
2017-06-06 19:15:50 +0530

    update formula
    update formula

--- www/js/myjs.js
+++ www/js/myjs.js
@@ -18,43 +18,39 @@ $(function(){
 			p_c = parseFloat($.trim($('#txtP_C').val()));
 			p_d = parseFloat($.trim($('#txtP_D').val()));
 			p_result = (p_c-(p_c*p_d)/100);
-			$('#txtP_E').val(p_result.toFixed(2));
+			$('#txtP_E').val(p_result.toFixed(0));
 		}
 		if($.trim($('#txtO_C').val()) != '' && $.trim($('#txtO_D').val()) != ''){
 			o_c = parseFloat($.trim($('#txtO_C').val()));
 			o_d = parseFloat($.trim($('#txtO_D').val()));
 			o_result = (o_c-(o_c*o_d)/100);
-			$('#txtO_E').val(o_result.toFixed(2));
+			$('#txtO_E').val(o_result.toFixed(0));
 		}
 		if($.trim($('#txtP_A').val())!= '' && $.trim($('#txtP_B').val())!='' && $.trim($('#txtP_E').val())!=''){
-			p_a = parseFloat($.trim($('#txtP_A').val()));
-			p_b = parseFloat($.trim($('#txtP_B').val()));
 			p_e = parseFloat($.trim($('#txtP_E').val()));
-			p_result = (p_e/(p_a*p_b))
-			$('#txtP_F').val(p_result.toFixed(4));
+			p_result = (p_e/(60))
+			$('#txtP_F').val(p_result.toFixed(2));
 		}
 		if($.trim($('#txtO_A').val())!= '' && $.trim($('#txtO_B').val())!='' && $.trim($('#txtO_E').val())!=''){
-			o_a = parseFloat($.trim($('#txtO_A').val()));
-			o_b = parseFloat($.trim($('#txtO_B').val()));
 			o_e = parseFloat($.trim($('#txtO_E').val()));
-			o_result = (o_e/(o_a*o_b))
-			$('#txtO_F').val(o_result.toFixed(4));
+			o_result = (o_e/(45))
+			$('#txtO_F').val(o_result.toFixed(2));
 		}
 		if($.trim($('#txtP_F').val())!=''){
 			p_f = parseFloat($.trim($('#txtP_F').val()));
 			p_result = p_f * 4046;
-			$('#txtP_G').val(p_result.toFixed(4));
+			$('#txtP_G').val(p_result.toFixed(2));
 		}
 		if($.trim($('#txtO_F').val())!=''){
 			o_f = parseFloat($.trim($('#txtO_F').val()));
 			o_result = o_f * 4046;
-			$('#txtO_G').val(o_result.toFixed(4));	
+			$('#txtO_G').val(o_result.toFixed(2));	
 		}
 		if($.trim($('#txtP_G').val()) != '' && $.trim($('#txtO_G').val()) != ''){
 			p = parseFloat($.trim($('#txtP_G').val()));
 			o = parseFloat($.trim($('#txtO_G').val()));
 			diff_ = p - o; 
-			$('#total_difference').html(diff_.toFixed(4));
+			$('#total_difference').html(diff_.toFixed(2));
 		}
 	});
 
